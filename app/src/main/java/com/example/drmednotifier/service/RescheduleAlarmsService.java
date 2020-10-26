4578356df07a81c26724d4ac91658491b246e02b
Yingjie Song <yingjiesong@cmail.carleton.ca>
2020-10-26 00:00:48 -0400

    Made the alarm feature working

--- app/src/main/java/com/example/drmednotifier/service/RescheduleAlarmsService.java
+++ app/src/main/java/com/example/drmednotifier/service/RescheduleAlarmsService.java
@@ -1,4 +1,50 @@
 package com.example.drmednotifier.service;
 
-public class RescheduleAlarmsService {
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LifecycleService;
+import androidx.lifecycle.Observer;
+
+import com.example.drmednotifier.data.Medication;
+import com.example.drmednotifier.data.MedicationRepository;
+
+import java.util.List;
+
+public class RescheduleAlarmsService extends LifecycleService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+        MedicationRepository medicationRepository = new MedicationRepository(getApplication());
+
+        medicationRepository.getMedicationsLiveData().observe(this, new Observer<List<Medication>>() {
+            @Override
+            public void onChanged(List<Medication> medications) {
+                for (Medication a : medications) {
+                    a.schedule(getApplicationContext());
+                }
+            }
+        });
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        super.onBind(intent);
+        return null;
+    }
 }
